fb8fd7b3870528c550454f74755dbbb3cd09f678
LuigiEBO <luigigames2903@gmail.com>
2024-03-23 17:53:04 -0300

    main

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
+// variáveis
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 let number = Math.round(Math.random() * 10)
 let xAttempts = 1
+// eventos
+btnTry.addEventListener("click", handleTryClick)
+btnReset.addEventListener("click", handleResetClick)
+document.addEventListener("keydown", function (e) {
+  console.log(e.key) 
+  if (e.key == 'Enter' && screen1.classList.contains("hide")) {
+    handleResetClick()
+  }
+})
+
+// funções callbacks
 function handleTryClick() {
   event.preventDefault()
   const inputNumber = document.querySelector("#inputNumber")
   if (Number(inputNumber.value) == number) {
-    screen1.classList.add("hide")
-    screen2.classList.remove("hide")
-    screen2.querySelector(
-      "h2"
-    ).innerText = `Acertou em ${xAttempts} tentativas`
+    toggleScreen()
+    screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`
   }
   inputNumber.value = ""
   xAttempts++
 }
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-btnTry.addEventListener("click", handleTryClick)
-btnReset.addEventListener("click", function () {
-  screen1.classList.remove("hide")
-  screen2.classList.add("hide")
+function handleResetClick() {
+  toggleScreen()
   xAttempts = 1
-})
+  number = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
